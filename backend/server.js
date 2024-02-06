b27820f95205c438f6302f1d26acfaa530f15402
nidhipawar5 <nidhipawar0504@gmail.com>
2024-02-06 23:44:18 +0530

    backend

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express"
+import dotenv from "dotenv"
+
+import authRoutes from "./routes/auth.routes.js"
+import connectToMongoDB from "./db/connectToMongoDB.js"
+
+const app = express()
+const PORT = process.env.PORT || 5001
+
+dotenv.config()
+
+app.use(express.json()) //to parse incoming requests with JSON payloads
+
+// app.get("/",(req,res) => {
+//     //root route https://localhost:5001/
+//     res.send("Hello world!!")
+// })
+
+//instead of app.get routes like used above, we use app.use to avoid routing everything separately
+
+app.use("/api/auth",authRoutes)
+
+app.listen(PORT, () => {
+    connectToMongoDB()
+    console.log(`Server is running on port ${PORT}`)
+})
