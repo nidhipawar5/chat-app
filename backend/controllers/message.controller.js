b6d240c87cd48911cbef266268c45edaade89e28
nidhipawar5 <nidhipawar0504@gmail.com>
2024-02-07 22:13:08 +0530

    get messages controller setup

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -38,7 +38,28 @@ export const sendMessage = async (req, res) => {
         res.status(201).json(newMessage)
 
     } catch (error) {
-        console.log("Error in message controller", error.message)
+        console.log("Error in sendMessage controller", error.message)
+        res.status(500).json({error: "Internal Server Error"})
+    }
+}
+
+export const getMessages = async (req, res) => {
+    try {
+        const{id:userToChatId} = req.params
+        const senderId = req.user._id
+
+        const conversation = await Conversation.findOne({
+            participants: { $all: [senderId, userToChatId]},
+        }).populate("messages") //mongoose method populate() -->gives not reference but actual messages
+
+        if(!conversation) return res.status(200).json([])
+
+        const messages = conversation.messages
+
+        res.status(200).json(messages)
+
+    } catch (error) {
+        console.log("Error in getMessages controller", error.message)
         res.status(500).json({error: "Internal Server Error"})
     }
 }
--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,9 +1,10 @@
-import { sendMessage } from "../controllers/message.controller.js"
+import { sendMessage, getMessages } from "../controllers/message.controller.js"
 import protectRoute  from "../middleware/protectRoute.js"
 import express from "express"
 
 const router = express.Router()
 
+router.get("/:id", protectRoute, getMessages)
 router.post("/send/:id", protectRoute, sendMessage)
 
 export default router
