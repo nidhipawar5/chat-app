17480f76d9e3c1c49eb14168af0e3c6f7ab87a95
nidhipawar5 <nidhipawar0504@gmail.com>
2024-02-07 22:39:26 +0530

    get users controller

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser"
 
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
+import userRoutes from "./routes/user.routes.js"
 
 import connectToMongoDB from "./db/connectToMongoDB.js"
 
@@ -24,6 +25,7 @@ app.use(cookieParser())
 
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
+app.use("/api/users",userRoutes)
 
 app.listen(PORT, () => {
     connectToMongoDB()
