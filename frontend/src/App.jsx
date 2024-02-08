fcca505f295ac88ff0b96ec0eab96be027bac38c
nidhipawar5 <nidhipawar0504@gmail.com>
2024-02-08 23:26:37 +0530

    sign up functionality on client side

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
+import {Route, Routes} from 'react-router-dom'
 import './App.css'
+import toast, { Toaster } from 'react-hot-toast';
 import Login from './pages/login/Login'
 import SignUp from './pages/signup/SignUp'
 import Home from './pages/home/Home'
@@ -7,7 +9,12 @@ function App() {
   return (
     <>
       <div className='p-4 h-screen flex items-center justify-center'>
-        <Home />
+        <Routes>
+          <Route path="/" element= {<Home />} />
+          <Route path="/login" element= {<Login />} />
+          <Route path="/signup" element= {<SignUp />} />
+        </Routes>
+        <Toaster/>
       </div>
     </>
   )
